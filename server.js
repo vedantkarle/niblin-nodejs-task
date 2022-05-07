a77cc5a9c1809b367ff9ccce5c2060a798331c57
vedant prasad priya karle <rays57529@gmail.com>
2022-05-07 22:32:29 +0530

    created auth routes

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ require("dotenv").config();
 
 const PORT = process.env.PORT || 5000;
 
+const authRoutes = require("./routes/auth.routes");
+
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+
 app.listen(PORT, () => {
 	console.log("listening on port", PORT);
 });
