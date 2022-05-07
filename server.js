c2664e0f28af506b3407105cde4ed21d63ffa66b
vedant prasad priya karle <rays57529@gmail.com>
2022-05-07 21:56:50 +0530

    Created basic folder structure

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+	console.log("listening on port", PORT);
+});
