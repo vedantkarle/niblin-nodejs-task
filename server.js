a43cddde9fa8f4a614497a54fc85e4ac7c040bd4
vedant prasad priya karle <karlevedant7@gmail.com>
2022-05-11 20:01:00 +0530

    added security measures

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const { connectDB } = require("./utils/db");
 const { notFound, errorHandler } = require("./middlewares/error");
+const helmet = require("helmet");
+const mongoSanitize = require('express-mongo-sanitize');
 require("dotenv").config();
 
 const PORT = process.env.PORT || 5000;
@@ -13,6 +15,15 @@ const app = express();
 connectDB();
 
 app.use(express.json());
+app.use(helmet());
+app.use(mongoSanitize());
+
+app.get("/",(req,res)=>{
+	res.json({
+		title:"Book-API",
+		author:"Vedant Karle"
+	})
+})
 
 app.use("/api/auth", authRoutes);
 app.use("/api/books", bookRoutes);
